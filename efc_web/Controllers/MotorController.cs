cb82510f48cc7dc2de23e5900447efb0eceab9e9
Thanasis Papoutsidakis <pwnzorbot4000@gmail.com>
2017-05-11 13:59:51 +0000

    Add motor status api call

--- efc_web/Controllers/MotorController.cs
+++ efc_web/Controllers/MotorController.cs
@@ -1,14 +1,32 @@
 ﻿using System.Web.Http;
+using EFC;
+
+using VegaDrive = EFC.Engines.VegaDrive_15P0087B5;
 
 namespace efc_web.Controllers
 {
     public class MotorController : ApiController
     {
+        private IEngine engine;
+
+        public MotorController()
+        {
+            engine = new VegaDrive("/dev/ttyUSB0", 1);
+        }
+
         [HttpGet]
         [Route("api/motor/status")]
         public IHttpActionResult Status()
         {
-            return Ok(42);
+            try
+            {
+                EngineStatus s = engine.Status();
+                return Ok(s.ToString());
+            }
+            catch (EngineMessageException)
+            {
+                return Ok("offline");
+            }
         }
     }
 }
